8c1faf5399307f416236ff2ab7a1cd874e8ab7cd
Aleksei Ivanov <aleksei.ivanov.qa@gmail.com>
2023-11-24 23:09:17 -0800

    new commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,59 @@
-import { NAV_LINKS } from "@/constants"
-import Image from "next/image"
-import Link from "next/link"
-import Button from "./Button"
+"use client"
+
+import { useState } from "react";
+import { NAV_LINKS } from "@/constants";
+import Image from "next/image";
+import Link from "next/link";
+import Button from "./Button";
 
 const Navbar = () => {
-  return (
-    <nav className="flexBetween max-container padding-container relative z-30 py-5">
-      <Link href="/">
-        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
-      </Link>
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-      <ul className="hidden h-full gap-12 lg:flex">
+  const handleMenuClick = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleCloseClick = () => {
+    setIsMenuOpen(false);
+  };
+
+  return (
+    <nav className="flexBetween max-container padding-container relative z-30 py-5 align-center">
+      {!isMenuOpen && (
+        <Link href="/">
+          <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+        </Link>
+      )}
+
+      <ul className={`h-full gap-12 lg:flex ${isMenuOpen ? 'flex-row justify-center' : 'hidden'}`}>
         {NAV_LINKS.map((link) => (
-          <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
-            {link.label}
-          </Link>
+          <li key={link.key}>
+            <Link href={link.href} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+              {link.label}
+            </Link>
+          </li>
         ))}
+        {isMenuOpen && (
+          <li>
+            <button onClick={handleCloseClick} className="close-button">
+              Close
+            </button>
+          </li>
+        )}
       </ul>
 
-      <div className="lg:flexCenter hidden">
+      {!isMenuOpen && (
+        <div className="inline-block cursor-pointer lg:hidden" onClick={handleMenuClick}>
+          <Image 
+            src="/menu.svg"
+            alt="menu"
+            width={32}
+            height={32}
+          />
+        </div>
+      )}
+
+      <div className={`lg:flexCenter hidden ${isMenuOpen ? 'justify-center' : ''}`}>
         <Button 
           type="button"
           title="Login"
@@ -26,16 +61,64 @@ const Navbar = () => {
           variant="btn_dark_green"
         />
       </div>
-
-      <Image 
-        src="menu.svg"
-        alt="menu"
-        width={32}
-        height={32}
-        className="inline-block cursor-pointer lg:hidden"
-      />
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { NAV_LINKS } from "@/constants"
+// import Image from "next/image"
+// import Link from "next/link"
+// import Button from "./Button"
+
+// const Navbar = () => {
+//   return (
+//     <nav className="flexBetween max-container padding-container relative z-30 py-5">
+//       <Link href="/">
+//         <Image src="/hilink-logo.svg" alt="logo" width={74} height={29} />
+//       </Link>
+
+//       <ul className="hidden h-full gap-12 lg:flex">
+//         {/* <ul className="hidden h-full gap-12 lg:flex"> */}
+//         {NAV_LINKS.map((link) => (
+//           <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+//             {link.label}
+//           </Link>
+//         ))}
+//       </ul>
+
+//       <div className="lg:flexCenter hidden">
+//         <Button 
+//           type="button"
+//           title="Login"
+//           icon="/user.svg"
+//           variant="btn_dark_green"
+//         />
+//       </div>
+
+//       <Image 
+//         src="menu.svg"
+//         alt="menu"
+//         width={32}
+//         height={32}
+//         className="inline-block cursor-pointer lg:hidden"
+//       />
+//     </nav>
+//   )
+// }
+
+// export default Navbar
